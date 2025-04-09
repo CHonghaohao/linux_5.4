6632db3ced1c56b5537c29bc607bb83daa99b82a
CHonghao <chonghao1108@163.com>
2025-04-09 17:54:08 +0800

    git init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Apr 9 11:36:14 CST 2025"
+#define LINUX_COMPILE_BY "chh"
+#define LINUX_COMPILE_HOST "CHonghao"
+#define LINUX_COMPILER "gcc version 10.3.1 20210621 (GNU Toolchain for the A-profile Architecture 10.3-2021.07 (arm-10.29))"
